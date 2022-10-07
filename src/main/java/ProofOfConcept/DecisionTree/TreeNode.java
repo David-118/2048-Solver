971bc33a18d9d4a71f6fbee684ff74181d954be3
Cook David (2020) ZJAC134 <zjac134@live.rhul.ac.uk>
2022-10-07 13:53:40 +0100

    Added weighted edges to decision tree proof of concept

--- src/main/java/ProofOfConcept/DecisionTree/TreeNode.java
+++ src/main/java/ProofOfConcept/DecisionTree/TreeNode.java
@@ -7,18 +7,23 @@ public class TreeNode
     private TreeNode[] children;
     private float score;
 
+    // While nicer interface will be used a weight of 1 is the same as unweighted.
+    private float weight;
+
 
     // Create leaf node
-    public TreeNode(float score)
+    public TreeNode(float weight, float score)
     {
         this.children = new TreeNode[0];
         this.score = score;
+        this.weight = weight;
     }
 
     // Create branch node
-    public TreeNode(TreeNode ... children)
+    public TreeNode(float weight, TreeNode ... children)
     {
         this.children = children;
+        this.weight = weight;
     }
 
     public int getChildCount()
@@ -34,5 +39,10 @@ public class TreeNode
     public float getScore()
     {
         return this.score;
+    }
+
+    public float getWeightedScore()
+    {
+        return this.score * this.weight;
     }
 }
--- src/main/tests/ProofOfConcept/DecisionTree/TreeNodeTest.java
+++ src/main/tests/ProofOfConcept/DecisionTree/TreeNodeTest.java
@@ -1,19 +1,26 @@
 package ProofOfConcept.DecisionTree;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TreeNodeTest
 {
+    private TreeNode root;
+    @BeforeEach
+    void setup()
+    {
+        this.root = new TreeNode(1f,
+                new TreeNode(1f, new TreeNode(0.5f, 1f), new TreeNode(0.5f, 2f)),
+                new TreeNode(1f, new TreeNode(0.1f, 3f), new TreeNode(0.8f, 4f), new TreeNode(0.1f, 5)),
+                new TreeNode(1f, new TreeNode(10f, 6f)));
+    }
+
     @Test
     void testCreateTree()
     {
-        TreeNode root = new TreeNode(
-                new TreeNode(new TreeNode(1), new TreeNode(2)),
-                new TreeNode(new TreeNode(3), new TreeNode(4), new TreeNode(5)),
-                new TreeNode(new TreeNode(6)));
-
         assertEquals(3, root.getChildCount());
         assertEquals(2, root.getChild(0).getChildCount());
         assertEquals(3, root.getChild(1).getChildCount());
@@ -24,14 +31,16 @@ public class TreeNodeTest
     @Test
     void testGetScore()
     {
-        TreeNode root = new TreeNode(
-                new TreeNode(new TreeNode(1), new TreeNode(2)),
-                new TreeNode(new TreeNode(3), new TreeNode(4), new TreeNode(5)),
-                new TreeNode(new TreeNode(6)));
-
-
         assertEquals(1f, root.getChild(0).getChild(0).getScore());
         assertEquals(4f, root.getChild(1).getChild(1).getScore());
         assertEquals(6f, root.getChild(2).getChild(0).getScore());
+    }
+
+    @Test
+    void testWeight()
+    {
+        assertEquals(0.5f, root.getChild(0).getChild(0).getWeightedScore());
+        assertEquals(3.2f, root.getChild(1).getChild(1).getWeightedScore());
+        assertEquals(60f, root.getChild(2).getChild(0).getWeightedScore());
     }
 }
