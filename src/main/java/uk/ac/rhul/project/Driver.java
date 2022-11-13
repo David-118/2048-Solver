c53e8975d4f2dbb19c069ce9b59bfdd9d82aa6bc
Cook David (2020) ZJAC134 <david.cook.2020@live.rhul.ac.uk>
2022-11-13 16:49:28 +0000

    Reverted changes

--- src/main/java/uk/ac/rhul/project/Driver.java
+++ src/main/java/uk/ac/rhul/project/Driver.java
@@ -1,18 +1,23 @@
 
 package uk.ac.rhul.project;
 
+import javafx.util.Pair;
+import uk.ac.rhul.project.benchmark.Benchmarker;
 import uk.ac.rhul.project.userInterface.MainController;
 import uk.ac.rhul.project.userInterface.MainModel;
 import uk.ac.rhul.project.userInterface.MainView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Driver
 {
-
-
     public static void main(String[] args)
     {
         MainView view = MainView.getInstance();
         MainModel model = new MainModel(4, 4);
         new MainController(model, view);
-   }
+    }
 }
