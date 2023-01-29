8de86ea4d60b41346f3a768f1de34d8dce6e8481
Cook David (2020) ZJAC134 <david.cook.2020@live.rhul.ac.uk>
2023-01-29 19:52:03 +0000

    Snake heurstic can now run in any of 8 possible directions..

--- program/src/main/java/uk/ac/rhul/project/heursitics/DynamicSnake.java
+++ program/src/main/java/uk/ac/rhul/project/heursitics/DynamicSnake.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 
 public class DynamicSnake implements Heuristic
 {
-    double[][] powers;
+    double[][][] powers;
 
     private final int rows, cols;
 
     public DynamicSnake(int row, int col)
     {
-        this.powers = new double[row][col];
+        this.powers = new double[8][row][col];
         this.rows = row; this.cols = col;
 
         for (int i = 0; i < row; i++)
@@ -20,23 +20,40 @@ public class DynamicSnake implements Heuristic
             for (int j = 0; j < col; j++)
             {
                 int k = i%2==0 ? j : row - j - 1;
-                this.powers[i][j] = Math.pow(4, col*i + k);
+                this.powers[0][i][j] = Math.pow(4, col*i + k);
+                this.powers[1][i][col - 1 - j] = this.powers[0][i][j];
+                this.powers[2][row - 1 - i][j] = this.powers[0][i][j];
+                this.powers[3][row - 1 - i][j] = this.powers[1][i][j];
+
+                int l = j%2==0 ? i : row - i - 1;
+
+                this.powers[4][i][j] = Math.pow(4, row*j + l);
+                this.powers[5][i][col - 1 - j] = this.powers[4][i][j];
+                this.powers[6][row - 1 - i][j] = this.powers[4][i][j];
+                this.powers[7][row - 1 - i][j] = this.powers[5][i][j];
+
+
             }
         }
     }
     @Override
     public double heuristic(GameState state)
     {
-        double sum = 0;
+        double maxSum = 0;
         int[][] grid = state.getGrid();
-        for (int i = 0; i < grid.length; i++)
+        for (int k = 0; k < 8; k++)
         {
-            for (int j = 0; j < grid[0].length; j++)
+            double sum = 0;
+            for (int i = 0; i < grid.length; i++)
             {
-                sum += grid[i][j] * this.powers[i][j];
+                for (int j = 0; j < grid[0].length; j++)
+                {
+                    sum += grid[i][j] * this.powers[k][i][j];
+                }
             }
+            if (sum > maxSum) maxSum = sum;
         }
-        return sum;
+        return maxSum;
     }
 
     @Override
